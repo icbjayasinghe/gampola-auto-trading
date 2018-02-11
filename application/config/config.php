450124a73c0584bb256738361c3d8c11547f5f33
Unknown <icbjayasinghe@gmail.com>
2018-02-11 11:54:12 +0530

    dns add

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://35.190.159.59/gampola-auto-trading/';
+$config['base_url'] = 'http://www.gampolaautotrading.com/gampola-auto-trading/';
 
 /*
 |--------------------------------------------------------------------------
